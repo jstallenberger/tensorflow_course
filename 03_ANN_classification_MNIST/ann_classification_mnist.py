2743e54fbb7b7c28d3551a385a0e7fb29f153abc
Jozsef Stallenberger <jstallenberger@gmail.com>
2020-12-07 01:39:51 +0100

    adds second part of the script with plots

--- 03_ANN_classification_MNIST/ann_classification_mnist.py
+++ 03_ANN_classification_MNIST/ann_classification_mnist.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 # Load in the data
 mnist = tf.keras.datasets.mnist
@@ -8,12 +9,12 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 print("x_train.shape: ", x_train.shape)
 
 # Build the model
-model = tf.keras.Sequential(
+model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28,28)),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10, activation='softmax')
-)
+])
 
 # Compile the model
 model.compile(optimizer='adam',
@@ -22,3 +23,23 @@ model.compile(optimizer='adam',
 
 # Train the model
 r = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10)
+
+# Plot loss per iteration
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+plt.savefig("/project/03_ANN_classification_MNIST/loss.png")
+plt.close()
+
+# Plot accuracy per iteration
+plt.plot(r.history['accuracy'], label='acc')
+plt.plot(r.history['val_accuracy'], label='val_acc')
+plt.legend()
+plt.savefig("/project/03_ANN_classification_MNIST/accuracy.png")
+plt.close()
+
+# Evaluate the model
+print(model.evaluate(x_test, y_test))
+
+# Save the model
+model.save("/project/03_ANN_classification_MNIST/model.h5")
