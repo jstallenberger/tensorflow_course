c58d450d9e24bb686924bc5c5c151e140f5b5117
Jozsef Stallenberger <jstallenberger@gmail.com>
2020-12-21 02:46:04 +0100

    adds CNN model for Fashion MNIST dataset

---
+++ 05_CNN_Fashion_MNIST/cnn.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+
+# https://github.com/tensorflow/tensorflow/issues/24496
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+
+# Load in the data
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+print("x_train.shape: ", x_train.shape)
+
+# The data is only 2-D
+# Convolution expects height x width x color
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("Reshaped x_train: ", x_train.shape)
+
+# Number of classes
+K = len(set(y_train))
+print("Number of classes: ", K)
+
+# Build the model using the Functional API
+i = Input(shape=x_train[0].shape)
+x = Conv2D(32, (3, 3), strides=2, activation='relu')(i)
+x = Conv2D(64, (3, 3), strides=2, activation='relu')(x)
+x = Conv2D(128, (3, 3), strides=2, activation='relu')(x)
+x = Flatten()(x)
+x = Dropout(0.2)(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(K, activation='softmax')(x)
+
+model = Model(i, x)
+
+# Compile and fit
+# Note: make sure you are using GPU for this
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+r = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=15)
+
+# Plot loss per iteration
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+plt.savefig("/project/05_CNN_Fashion_MNIST/loss.png")
+plt.close()
+
+# Plot accuracy per iteration
+plt.plot(r.history['accuracy'], label='acc')
+plt.plot(r.history['val_accuracy'], label='val_acc')
+plt.legend()
+plt.savefig("/project/05_CNN_Fashion_MNIST/accuracy.png")
+plt.close()
