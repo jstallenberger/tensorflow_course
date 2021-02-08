750e7d295bb3816f7f542a2987493cf6f10673bf
Jozsef Stallenberger <jstallenberger@gmail.com>
2021-02-08 01:06:00 +0100

    corrects data loader

--- 07_CNN_CIFAR-10_improved/random_misclassified_sample.py
+++ 07_CNN_CIFAR-10_improved/random_misclassified_sample.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load in the data
-mnist = tf.keras.datasets.mnist
+cifar10 = tf.keras.datasets.cifar10
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
+y_train, y_test = y_train.flatten(), y_test.flatten() # need to flatten as targets ar 2D arrays, Nx1 instead of N
 
 # Load the model
 model = tf.keras.models.load_model("/project/07_CNN_CIFAR-10_improved/model.h5")
