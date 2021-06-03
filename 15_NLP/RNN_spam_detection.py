a243490ab3760356c8f9125049c9e615f0692e5d
Jozsef Stallenberger <jstallenberger@gmail.com>
2021-06-03 22:52:28 +0200

    adds spam detection script

---
+++ 15_NLP/RNN_spam_detection.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Dense, Input, GlobalMaxPooling1D
+from tensorflow.keras.layers import LSTM, Embedding
+from tensorflow.keras.models import Model
+
+
+df = pd.read_csv('/project/15_NLP/spam.csv', encoding='ISO-8859-1')
+# print(df.head())
+
+# Drop unnecessary columns
+df = df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
+# print(df.head())
+
+# Rename columns to something better
+df.columns = ['labels', 'data']
+print(df.head())
+
+# Create binary labels
+df['b_labels'] = df['labels'].map({'ham': 0, 'spam': 1})
+Y = df['b_labels'].values
+
+# Split up the data
+df_train, df_test, Ytrain, Ytest = train_test_split(df['data'], Y, test_size=0.33)
+
+# Convert sentences to sequences
+MAX_VOCAB_SIZE=20000
+tokenizer = Tokenizer(num_words=MAX_VOCAB_SIZE)
+tokenizer.fit_on_texts(df_train)
+sequences_train = tokenizer.texts_to_sequences(df_train)
+sequences_test = tokenizer.texts_to_sequences(df_test)
+
+# Get word -> integer mapping
+word2idx = tokenizer.word_index
+V = len(word2idx)
+print("Found %s unique tokens." % V)
+
+# Pad sequences so we get a N x T matrix
+data_train = pad_sequences(sequences_train)
+print("Shape of data train tensor: ", data_train.shape)
+
+# get sequence length
+T = data_train.shape[1]
+
+data_test = pad_sequences(sequences_test, maxlen=T)
+print("Shape of data test tensor: ", data_test.shape)
+
+
+# Create the model
+
+# We get to choose embedding dimensionality
+D = 20
+
+# Hidden state dimensionality
+M = 15
+
+# Note: we actually want to the size of the embedding to (V + 1) x D,
+# because the first index starts with 1 and not 0.
+# Thus, if the final index of the embedding matrix is V,
+# then it actually must have size V + 1.
+
+i = Input(shape=(T,))
+x = Embedding(V + 1, D)(i)
+x = LSTM(M, return_sequences=True)(x)
+x = GlobalMaxPooling1D()(x)
+x = Dense(1, activation='sigmoid')(x)
+
+model = Model(i, x)
+
+# Compile and fit
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
+
+print("Training model...")
+r = model.fit(
+    data_train,
+    Ytrain,
+    epochs=10,
+    validation_data=(data_test, Ytest)
+)
+
+# Plot loss per iteration
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+plt.savefig('/project/15_NLP/loss.png')
+plt.close()
+
+# Plot accuracy per iteration
+plt.plot(r.history['accuracy'], label='acc')
+plt.plot(r.history['val_accuracy'], label='val_acc')
+plt.legend()
+plt.savefig('/project/15_NLP/accuracy.png')
+plt.close()
