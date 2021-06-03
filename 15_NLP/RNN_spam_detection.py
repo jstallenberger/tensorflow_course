14f188309cbe8847966296f43b0171dc90a5d9ad
Jozsef Stallenberger <jstallenberger@gmail.com>
2021-06-03 23:25:37 +0200

    modifies plt figure names

--- 15_NLP/RNN_spam_detection.py
+++ 15_NLP/RNN_spam_detection.py
@@ -91,12 +91,12 @@ r = model.fit(
 plt.plot(r.history['loss'], label='loss')
 plt.plot(r.history['val_loss'], label='val_loss')
 plt.legend()
-plt.savefig('/project/15_NLP/loss.png')
+plt.savefig('/project/15_NLP/RNN_loss.png')
 plt.close()
 
 # Plot accuracy per iteration
 plt.plot(r.history['accuracy'], label='acc')
 plt.plot(r.history['val_accuracy'], label='val_acc')
 plt.legend()
-plt.savefig('/project/15_NLP/accuracy.png')
+plt.savefig('/project/15_NLP/RNN_accuracy.png')
 plt.close()
